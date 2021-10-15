d6d6e164c5d774ae7ee1f06c000150cd52736327
SuperCat2812 <Sashka281205@gmail.com>
2021-10-15 18:50:49 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 let a = document.getElementById("button");
-let b = document.getElementById("body");
+let b = document.getElementById("divbody");
 let c = document.getElementById("img");
 
 a.onclick = function () {
   alert("нажата");
-  let leftx = Math.floor(Math.random() * 600);
-  let topy = Math.floor(Math.random() * 600);
+  let leftx = Math.floor(Math.random() * 300);
+  let topy = Math.floor(Math.random() * 300);
   a.style.left = leftx + "px";
   a.style.top = topy + "px";
   a.style.backgroundColor = "red";
   b.style.backgroundColor = "black";
-  b.style.border = "dashed white";
+  b.style.border = "3px dashed white";
   c.src = "./img/img2.jfif";
 };
